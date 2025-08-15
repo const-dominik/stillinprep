f5948219ba61e30e23b0849c7ec53e735808491b
dominik.kielbowicz <dominik.kielbowicz19@gmail.com>
2025-08-15 15:58:11 +0200

    feat: added privacy policy

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,43 @@
+const PrivacyPolicy = () => {
+    return (
+        <div>
+            <pre>
+                {`Privacy Policy
+Last updated: 8/15/2025
+
+1. Introduction
+This Privacy Policy explains how Still In Prep (“we”, “our”, or “us”) handles information when you use our application.
+
+2. Information We Collect
+When you sign in using one of our supported login methods (Google, GitHub, or email/password), we collect:
+- Your email address (stored in our database for authentication purposes)
+
+We do not store or process any other personal data.
+We do not access your Gmail messages, contacts, or any other data from your Google account.
+
+3. How We Use Information
+We use your email address only to:
+- Authenticate your login
+- Identify your account in our system
+
+4. Data Sharing
+We do not share, sell, or rent your information to third parties.
+Your data is only used to provide the features of the app.
+
+5. Data Security
+We use industry-standard security measures to protect your information.
+
+6. Your Choices
+You can revoke our access to your Google account or GitHub account at any time from your respective account settings:
+- Google: https://myaccount.google.com/permissions
+- GitHub: https://github.com/settings/connections/applications
+
+7. Contact Us
+If you have questions about this Privacy Policy, contact:
+stillinprep@gmail.com`}
+            </pre>
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
