d704d868597709d5aee883ffa2ef4e625c818fec
dominik.kielbowicz <dominik.kielbowicz19@gmail.com>
2025-08-16 16:38:19 +0200

    feat: speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.scss";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 {children}
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
